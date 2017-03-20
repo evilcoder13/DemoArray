8d746d7f1020ea821bc1f76e27b8a48753c838f8
Thang <thangdm@d3plus.com>
2017-03-20 19:44:49 +0700

    Them comment

--- ArrayDemo/Class1.cs
+++ ArrayDemo/Class1.cs
@@ -8,11 +8,24 @@ namespace ExtendedMath1
 {
     public class Class1
     {
+        /// <summary>
+        /// Cong 2 so
+        /// </summary>
+        /// <param name="a"></param>
+        /// <param name="b"></param>
+        /// <returns></returns>
         internal static int Add(int a, int b)
         {
             //return a + b;
             return Add(a, b, 0);
         }
+        /// <summary>
+        /// Cong 3 so
+        /// </summary>
+        /// <param name="a"></param>
+        /// <param name="b"></param>
+        /// <param name="c"></param>
+        /// <returns></returns>
         internal static int Add(int a, int b, int c)
         {
             return a + b + c;
