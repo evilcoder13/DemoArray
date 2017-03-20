fb70aecb285a396b27c6faf62dcc0e0de4b349c6
Thang <thangdm@d3plus.com>
2017-03-20 19:40:55 +0700

    Khoi tao project

---
+++ ArrayDemo/Class1.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExtendedMath1
+{
+    public class Class1
+    {
+        internal static int Add(int a, int b)
+        {
+            //return a + b;
+            return Add(a, b, 0);
+        }
+        internal static int Add(int a, int b, int c)
+        {
+            return a + b + c;
+        }
+
+        void ViewData(int page, string orderby)
+        {
+            //return SearchData(page, orderby, "");
+        }
+
+        void SearchData(int page, string orderby, string keyword)
+        {
+            //...
+        }
+
+    }
+}
---
+++ ArrayDemo/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ArrayDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] studentNames = new string[5];
+            studentNames[0] = "Thang";
+            studentNames[1] = "Thanh";
+            studentNames[2] = "Trang";
+            studentNames[3] = "Ha";
+            studentNames[4] = "Huyen";
+            foreach (string s in studentNames)
+                Console.WriteLine(s);
+            Console.ReadLine();
+
+            string[] students = { "Thang", "Thanh", "Trang", "Ha", "Huyen" };
+            for (int i = 0; i < students.Length; i++)
+                Console.WriteLine(i+":"+students[i]);
+            Console.ReadLine();
+
+            string[,] studentInfos = new string[5, 2];
+            studentInfos[0, 0] = "Thang";
+            studentInfos[0, 1] = "10A";
+            studentInfos[1, 0] = "Thanh";
+            studentInfos[1, 1] = "10B";
+            studentInfos[2, 0] = "Hoang";
+            studentInfos[2, 1] = "10C";
+            studentInfos[3, 0] = "Trang";
+            studentInfos[3, 1] = "10A";
+            studentInfos[4, 0] = "Huyen";
+            studentInfos[4, 1] = "10C";
+            for (int i = 0; i < 5; i++)
+                DisplayStudentInfo(studentInfos[i, 0], studentInfos[i, 1]);
+            DisplayStudentInfo("test");
+            Console.WriteLine(ExtendedMath1.Class1.Add(5, 10));
+            Console.ReadLine();
+
+        }
+
+        static void DisplayStudentInfo(string name, string classname="Waiting")
+        {
+            Console.WriteLine("Ten: " + name + " - Lop: " + classname);
+        }
+    }
+}
